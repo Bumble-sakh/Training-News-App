d9197cfd3526d455a2171822c3ce930d2fbf79f2
Bumble <bumble.sakh@gmail.com>
2021-12-19 17:09:11 +1100

    refactor: prettier code

--- app.js
+++ app.js
@@ -1,167 +1,168 @@
-const http = customHttp();
+const http = customHttp()
 
-const newsService = (function() {
-  const apiKey = '5c2b80530ab74210ae5ab70922bb63a4';
-  const apiUrl = 'https://newsapi.org/v2/';
+const newsService = (function () {
+  const apiKey = '5c2b80530ab74210ae5ab70922bb63a4'
+  const apiUrl = 'https://newsapi.org/v2/'
 
   return {
     topHeadlines(country = 'ru', cb) {
-      http.get(`${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`, cb);
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        cb
+      )
     },
     everything(query, cb) {
-      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb)
     },
   }
-})();
+})()
 
 //UI
-const form = document.forms['newsControls'];
-const countrySelect = form.elements['country'];
-const searchInput = form.elements['search'];
-const spinnerContainer = document.querySelector('.spinner-container');
-const newsContainer = document.querySelector('.news-container');
-const toastContainer = document.querySelector('.toast-container');
+const form = document.forms['newsControls']
+const countrySelect = form.elements['country']
+const searchInput = form.elements['search']
+const spinnerContainer = document.querySelector('.spinner-container')
+const newsContainer = document.querySelector('.news-container')
+const toastContainer = document.querySelector('.toast-container')
 
 //Events
-document.addEventListener('DOMContentLoaded', e => {
-  loadNews();
-});
+document.addEventListener('DOMContentLoaded', (e) => {
+  loadNews()
+})
 
-countrySelect.addEventListener('change', e => {
-    country = countrySelect.value;
-    loadNews();
-});
+countrySelect.addEventListener('change', (e) => {
+  country = countrySelect.value
+  loadNews()
+})
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  countrySelect.value = '';
-  loadNews();
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  countrySelect.value = ''
+  loadNews()
+})
 
 function customHttp() {
-    return {
-      get(url, cb) {
-        try {
-          const xhr = new XMLHttpRequest();
-          xhr.open('GET', url);
-          xhr.send();
-      
-          xhr.addEventListener('load', () => {
-            if(Math.floor(xhr.status / 100) !== 2) {
-              cb(`Error. Status code: ${xhr.status}`, xhr);
-              return ;
-            }
-            const response = JSON.parse(xhr.responseText);
-            cb(null, response);
-          });
-      
-          xhr.addEventListener('error', () => {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-          });
-        } catch (error) {
-          cb(`Error. ${error}`);
-        }  
-      },
-      post(url, body, headers, cb) {
-        try {
-          const xhr = new XMLHttpRequest();
-          xhr.open('POST', url);
-          
-          // for (const header in headers) {
-          //   xhr.setRequestHeader(header, headers[header]);
-          // }
-  
-          Object.entries(headers).forEach(([key, value]) => {
-            xhr.setRequestHeader(key, value);
-          })  
-          
-          xhr.send(JSON.stringify(body));
-      
-          xhr.addEventListener('load', () => {
-            if(Math.floor(xhr.status / 100) !== 2) {
-              cb(`Error. Status code: ${xhr.status}`, xhr);
-              return ;
-            }
-            const response = JSON.parse(xhr.responseText);
-            cb(null, response);
-          });
-      
-          xhr.addEventListener('error', () => {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-          });
-        } catch (error) {
-          cb(`Error. ${error}`);
-        }  
-      },
-    }
-  }
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', url)
+        xhr.send()
 
-function loadNews() {
-  const country = countrySelect.value;
-  const query = searchInput.value;
-  searchInput.value = '';
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr)
+            return
+          }
+          const response = JSON.parse(xhr.responseText)
+          cb(null, response)
+        })
 
-  addSpinner();  
-  
-  if(!query) {
-    newsService.topHeadlines(country, onGetResponse);
-  } else {
-    newsService.everything(query, onGetResponse);
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr)
+        })
+      } catch (error) {
+        cb(`Error. ${error}`)
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest()
+        xhr.open('POST', url)
+
+        // for (const header in headers) {
+        //   xhr.setRequestHeader(header, headers[header]);
+        // }
+
+        Object.entries(headers).forEach(([key, value]) => {
+          xhr.setRequestHeader(key, value)
+        })
+
+        xhr.send(JSON.stringify(body))
+
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr)
+            return
+          }
+          const response = JSON.parse(xhr.responseText)
+          cb(null, response)
+        })
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr)
+        })
+      } catch (error) {
+        cb(`Error. ${error}`)
+      }
+    },
   }
 }
 
-function onGetResponse(err, res) { 
-  delSpinner();
-  
-  if(err) {
-    showMessage(err, 'bg-danger');
-    return;
+function loadNews() {
+  const country = countrySelect.value
+  const query = searchInput.value
+  searchInput.value = ''
+
+  addSpinner()
+
+  if (!query) {
+    newsService.topHeadlines(country, onGetResponse)
+  } else {
+    newsService.everything(query, onGetResponse)
   }
-  if(!res.articles.length) {
-    const msg = 'No news on the selected topic';
-    showMessage(msg, 'bg-warning');
-    return;
+}
+
+function onGetResponse(err, res) {
+  delSpinner()
+
+  if (err) {
+    showMessage(err, 'bg-danger')
+    return
+  }
+  if (!res.articles.length) {
+    const msg = 'No news on the selected topic'
+    showMessage(msg, 'bg-warning')
+    return
   }
 
-  renderNews(res.articles);
+  renderNews(res.articles)
 }
 
 function renderNews(news) {
-  clearNewsContainer();
+  clearNewsContainer()
 
-  news.forEach(newsItem => {
-    const el = newsTemplate(newsItem);
-    newsContainer.insertAdjacentHTML('afterbegin', el);
-  });
+  news.forEach((newsItem) => {
+    const el = newsTemplate(newsItem)
+    newsContainer.insertAdjacentHTML('afterbegin', el)
+  })
 }
 
 function addSpinner() {
-  spinnerContainer.classList.remove('d-none');
+  spinnerContainer.classList.remove('d-none')
 }
 
 function delSpinner() {
-  spinnerContainer.classList.add('d-none');
+  spinnerContainer.classList.add('d-none')
 }
 
 function clearNewsContainer() {
-
   while (newsContainer.children.length) {
     newsContainer.removeChild(newsContainer.firstChild)
   }
-
 }
 
 function showMessage(msg, type = 'bg-primary') {
-  const toastId = `f${(+new Date).toString(16)}`;
-  const element = toastTemplate(toastId, msg, type);
-  toastContainer.insertAdjacentHTML('afterbegin', element);
+  const toastId = `f${(+new Date()).toString(16)}`
+  const element = toastTemplate(toastId, msg, type)
+  toastContainer.insertAdjacentHTML('afterbegin', element)
 
-  const toastElement = document.getElementById(toastId);
-  const toast = new bootstrap.Toast(toastElement);
-  toast.show();
+  const toastElement = document.getElementById(toastId)
+  const toast = new bootstrap.Toast(toastElement)
+  toast.show()
 }
 
-function newsTemplate({ urlToImage, title, description, url}) {
+function newsTemplate({ urlToImage, title, description, url }) {
   return `
     <div class="col-12 col-md-6 col-xl-4 my-2">
       <div class="card h-100">
@@ -175,7 +176,7 @@ function newsTemplate({ urlToImage, title, description, url}) {
           </div>
       </div>
     </div>
-  `;
+  `
 }
 
 function toastTemplate(id, msg, type) {
@@ -188,5 +189,5 @@ function toastTemplate(id, msg, type) {
         <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
       </div>
     </div>
-  `;
+  `
 }
